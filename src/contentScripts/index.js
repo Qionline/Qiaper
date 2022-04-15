c655a4ccee899afa052e41eeddc10dea6acd54e3
宋昊天 <5516023@qq.com>
2022-04-15 13:22:01 +0800

    :zap: chore: 优化脚本加载速度

--- src/contentScripts/index.js
+++ src/contentScripts/index.js
@@ -4,19 +4,17 @@
 // extensionGlobals.innerText = `window.QIAPER_EXTENTION_ID = "${chrome.runtime.id}";`
 // document.documentElement.appendChild(extensionGlobals)
 
-const ajaxHookScript = document.createElement('script')
-ajaxHookScript.src = chrome.runtime.getURL('contentScripts/ajax-hook.min.js')
-document.documentElement.appendChild(ajaxHookScript)
-
 const interceptScript = document.createElement('script')
 interceptScript.src = chrome.runtime.getURL('contentScripts/intercept.js')
 document.documentElement.appendChild(interceptScript)
 
 // 注入 mock list
-chrome.storage.local.get(["mockRequestList"], function (res) {
-  const value = res.mockRequestList ? JSON.parse(res.mockRequestList) : []
-  postMessage({ action: 'UPDATE_MOCK_LIST', value });
-})
+interceptScript.addEventListener('load', () => {
+  chrome.storage.local.get(["mockRequestList"], function (res) {
+    const value = res.mockRequestList ? JSON.parse(res.mockRequestList) : []
+    postMessage({ action: 'UPDATE_MOCK_LIST', value });
+  })
+});
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "UPDATE_MOCK_LIST") {
